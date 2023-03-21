3e2415f2271ead189c900b277fbfaf368a8d21d4
zituhossain5 <isminhaz3@gmail.com>
2023-03-21 06:39:12 +0600

    modified problem 4 and update style.css file

--- script.js
+++ script.js
@@ -26,20 +26,27 @@ for(i = 1; i <= 10; i++) {
 }
 
 // 4. Print all the multiplication tables with numbers from 1 to 10:
+var manyTables = ''; // change variable name to manyTables to reflect that we will have multiple tables
+var a;
 var j;
-for(j = 1; j <= 2; j++) {
-    printTable(j);
+
+function printTable(number) {
+  var table = '<div class="table-container">'; // wrap each table in a container div
+  for (a = 1; a <= 10; a++) {
+    var result = number * a;
+    var row = '<div>' + number + ' * ' + a + ' = ' + result + '</div>';
+    table += row;
+  }
+  table += '</div>'; // close the container div
+  manyTables += table;
 }
 
-var manyTable;
-var a
-function printTable(number) {
-    for(a = 1; a <= 10; a++) {
-        var results = number + ' * ' + a + ' = ' + number * a;
-        manyTable += results + '<br>';
-        document.getElementById('p4').innerHTML = manyTable;
-    }
+for (j = 1; j <= 10; j++) {
+  printTable(j);
 }
+
+document.getElementById('p4').innerHTML = manyTables;
+
 
 // 5. Calculate the sum of numbers from 1 to 10:
 var i;
